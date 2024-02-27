a4f785ed84ed8b20295fe5514ecf24aab1a225da
Lubov Krasheninnikova <altukhovalv@yandex.ru>
2024-02-27 14:27:23 +0300

    New fails

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int amount = 20690;
+    int x = 20;
+
+    int bonus = amount / x;
+    System.out.println(bonus);
+    }
+}
