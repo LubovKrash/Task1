a7afe2a6352a5a572b9a3097e5315e61c08067c8
Lubov Krasheninnikova <altukhovalv@yandex.ru>
2024-02-27 22:36:57 +0300

    New changes

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    int amount = 20690;
-    int x = 20;
+        int amount = 20690;
+        int mileCost = 20;
 
-    int bonus = amount / x;
-    System.out.println(bonus);
+        int bonus = amount / mileCost;
+        System.out.println(bonus);
     }
 }
